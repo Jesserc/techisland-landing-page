4c9c91318b9ca0abde729afa9fc517cf135913f8
Jesse Raymond <nodejesse@gmail.com>
2022-04-05 14:24:15 +0100

    techisland build

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 const useStyles = makeStyles((theme) => ({
 	root: {
 		minHeight: "100vh",
+		minWidth: "100vw",
 		backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg.jpg"})`,
 		backgroundRepeat: "no-repeat",
 		backgroundSize: "cover",
--- src/components/Header.js
+++ src/components/Header.js
@@ -17,28 +17,48 @@ const useStyles = makeStyles((theme) => ({
 	appbarWrapper: {
 		width: "80%",
 		margin: "0 auto",
+		[theme.breakpoints.down("sm")]: {
+			width: "90%",
+			margin: "0 auto",
+		},
 	},
 	appbarTitle: {
 		flexGrow: "1",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1.2rem",
+		},
 	},
 	aboutUs: {
 		color: "#fff",
 		fontSize: "2rem",
 		cursor: "pointer",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1.2rem",
+		},
 	},
 	colorText: {
 		color: "#5AFF3D",
 	},
 	container: {
 		textAlign: "center",
+		[theme.breakpoints.down("sm")]: {
+			textAlign: "left",
+			width: "80%",
+		},
 	},
 	title: {
 		color: "#fff",
 		fontSize: "4.5rem",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "3rem",
+		},
 	},
 	subTitle: {
 		color: "#fff",
 		fontSize: "1.5rem",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1.2rem",
+		},
 	},
 
 	goDown: {
@@ -71,7 +91,7 @@ export default function Header() {
 					Welcome to <br /> Tech
 					<span className={classes.colorText}>Island.</span>
 				</h1>
-				<p className={classes.subTitle}>resorts just for techies..</p>
+				<p className={classes.subTitle}>resort for techies..</p>
 			</div>
 		</div>
 	);
